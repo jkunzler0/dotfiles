f5ddc41e5dfb848ccf6092a8bc0be80622dfb1ba
Joseph Kunzler <jkunzler@inductor.ai>
2024-05-12 17:24:20 -0700

    nvim keymap updates

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,14 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("i", "jk", "<ESC>", { noremap = true, silent = true, desc = "<ESC>" })
 
 -- Perusing code faster with K and J
 vim.keymap.set({ "n", "v" }, "K", "5kzz", { noremap = true, desc = "Up faster" })
 vim.keymap.set({ "n", "v" }, "J", "5jzz", { noremap = true, desc = "Down faster" })
 
 -- Remap K and J
-vim.keymap.set({ "n", "v" }, "<leader>k", "K", { noremap = true, desc = "Keyword" })
+-- K is remaped in plugins/lazy_remap.lua
 vim.keymap.set({ "n", "v" }, "<leader>j", "J", { noremap = true, desc = "Join lines" })
 
 -- Save file
---
+++ .config/nvim/lua/plugins/lazy_remap.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "folke/noice.nvim",
+    opts = {
+      lsp = {
+        signature = {
+          auto_open = {
+            enabled = false,
+          },
+        },
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    init = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      -- Remap hover
+      keys[#keys + 1] = { "K", false }
+      keys[#keys + 1] = { "gk", vim.lsp.buf.hover, desc = "Hover" }
+    end,
+  },
+}
+
